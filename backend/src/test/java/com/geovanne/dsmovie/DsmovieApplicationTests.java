e3e52da85bf942aa77e43b67e24c9933112fd3db
Geovanne Santos da Silva <santosdasilvag12@gmail.com>
2022-01-13 20:45:54 -0300

    Project created

---
+++ backend/src/test/java/com/geovanne/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geovanne.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
