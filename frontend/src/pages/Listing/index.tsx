cd96ae47be00f6b72c25db876e32fafd1f460bc8
Geovanne Santos da Silva <santosdasilvag12@gmail.com>
2022-01-16 20:58:21 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,26 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
+    const [pageNumber, setPageNunber] = useState(0);
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNunber(data.number)
         });
-
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
